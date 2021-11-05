9fa83503b861287403697ecaef86d7bb11d423ee
DesktopPC <mpalacz00@gmail.com>
2021-11-05 16:48:12 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean czyKontynuowacDzialanieProgramu = true;
+        char klasaSieci;
+        short[] adresSieci = new short[4];
+        short[][] podsieci = new short[3][4]; //pierwszy nawias - podsieci, drugi nawias - hosty
+    }
+}
